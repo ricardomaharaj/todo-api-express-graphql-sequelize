34542b13a971666cd6c9846a3521e7d61065df37
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-07-08 13:18:02 -0400

    cleanup formatting, port checking, use db file

--- app.js
+++ app.js
@@ -1,64 +1,49 @@
-let express = require("express")
-let { buildSchema } = require("graphql")
-let { graphqlHTTP } = require("express-graphql")
-let { Sequelize, DataTypes } = require("sequelize")
+let express = require('express')
+let { buildSchema } = require('graphql')
+let { graphqlHTTP } = require('express-graphql')
+let { Sequelize, DataTypes } = require('sequelize')
+let { readFileSync } = require('fs')
 
-let sequelize = new Sequelize("sqlite::memory:", { logging: false })
+let sequelize = new Sequelize('sqlite:db.sqlite', { logging: false })
 
 let Todo = sequelize.define('Todo', {
-  title: DataTypes.STRING,
-  completed: DataTypes.BOOLEAN
+    title: DataTypes.STRING,
+    completed: DataTypes.BOOLEAN
 })
+
+const PORT = process.env.PORT || 4000
 
 sequelize.sync()
 
-let schema = buildSchema(`
-  type Todo {
-    id: ID
-    title: String
-    completed: String
-    createdAt: String
-    updatedAt: String
-  }
-  type Query {
-    findAll: [Todo]
-    findById(id: ID): Todo
-  }
-  type Mutation {
-    create(title: String): Todo
-    update(id: ID, title: String): Todo
-    markComplete(id: ID): Todo
-    delete(id: ID): Todo
-  }
-`)
+let schema = buildSchema(readFileSync(__dirname + '/schema.gql').toString('utf-8'))
 
 let resolvers = {
-  findAll: async () => {
-    return await Todo.findAll()
-  },
-  findById: async ({ id }) => {
-    return await Todo.findByPk(id)
-  },
-  create: async ({ title }) => {
-    return await Todo.create({ title, completed: false })
-  },
-  update: async ({ id, title }) => {
-    let todo = await Todo.findByPk(id)
-    await todo.update({ title })
-    return todo
-  },
-  delete: async ({ id }) => {
-    let todo = await Todo.findByPk(id)
-    await Todo.destroy({ where: { id } })
-    return todo
-  },
-  markComplete: async ({ id }) => {
-    let todo = await Todo.findByPk(id)
-    await todo.update({ completed: true })
-    return todo
-  }
+    findAll: async () => {
+        return await Todo.findAll()
+    },
+    findById: async ({ id }) => {
+        return await Todo.findByPk(id)
+    },
+    create: async ({ title }) => {
+        return await Todo.create({ title, completed: false })
+    },
+    update: async ({ id, title }) => {
+        let todo = await Todo.findByPk(id)
+        await todo.update({ title })
+        return todo
+    },
+    delete: async ({ id }) => {
+        let todo = await Todo.findByPk(id)
+        await Todo.destroy({ where: { id } })
+        return todo
+    },
+    markComplete: async ({ id }) => {
+        let todo = await Todo.findByPk(id)
+        await todo.update({ completed: true })
+        return todo
+    }
 }
 
 let app = express()
 app.use('/', graphqlHTTP({ schema, rootValue: resolvers, graphiql: true }))
-app.listen(4000, () => { console.log('http://localhost:4000') })
+app.listen(PORT, () => console.log('http://localhost:' + PORT))
